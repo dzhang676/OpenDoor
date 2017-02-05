37c0749c610904e12149a00469c7343b15d7781b
Austin Atmaja <austin.atmaja@gmail.com>
2017-02-05 02:59:34 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,17 +1,7 @@
-const config = {
-  apiKey: "AIzaSyC3mq8GoVIYRSDMCDtqPEMd837Kizv2EzA",
-  authDomain: "opendoor-acf3a.firebaseapp.com",
-  databaseURL: "https://opendoor-acf3a.firebaseio.com",
-  storageBucket: "opendoor-acf3a.appspot.com",
-  messagingSenderId: "903661368930",
-};
-
-firebase.initializeApp(config);
-
 
 function checkStudent(form){
 	if (form.email.value == "atatmaja" && form.pass.value == "foosball"){
-		window.open("https://www.youtube.com/")
+		window.open("OpenDoorHome.html")
 	}
 	else{
 		alert("Incorrect Login Credentials")
@@ -20,11 +10,28 @@ function checkStudent(form){
 
 function checkFaculty(form){
 	if (form.email.value == "sbirkett" && form.pass.value == "partytricks"){
-		window.open("OpenDoorHome.html")
+		window.open("OpenDoorProfessor.html")
 	}
 	else{
 		alert("Incorrect Login Credentials")
 	}
 }
+
+var profcheckin = document.getElementById("profcheckin");
+
+var profcheckout = document.getElementById("profcheckout");
+
+var profStatusRef = firebase.database().ref();
+
+function checkinprof(){
+	profStatusRef.child("Status").set("Available");
+}
+
+function checkoutprof(){
+	var profoutRef = firebase.database().ref();
+	profStatusRef.child("Status").set("Not In Office");
+}
+
+
 
 
